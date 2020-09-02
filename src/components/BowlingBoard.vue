bd88c31f8ec3d9a279e14f989806bdbef4bb81db
Caja Petersen <caja@pwnt.dk>
2020-09-02 16:51:03 +0200

    use axios baseurl

--- src/components/BowlingBoard.vue
+++ src/components/BowlingBoard.vue
@@ -6,28 +6,27 @@
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-class-component';
-import axios from 'axios';
+import { Vue } from "vue-class-component";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://13.74.31.101";
 
 export default class BowlingBoard extends Vue {
   data() {
     return {
       points: Array,
-      token: String
-      }
-      
+      token: String,
+    };
   }
 
   points!: Array<number>;
   token!: string;
 
-async created () {
-  await axios.get('http://13.74.31.101/api/points')
-    .then(response => {
+  async created() {
+    await axios.get("/api/points", {}).then(response => {
       this.points = response.data.points,
-      this.token = response.data.token
-    })
+      this.token = response.data.token;
+    });
   }
 }
-
 </script>
